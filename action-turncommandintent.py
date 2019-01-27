3ddae46f67432b4b47fa0a1ecf7af7499a8e47c2
Gunasekar T <gunasekart@Gunasekars-MacBook-Pro.local>
2019-01-27 19:07:16 +0530

    turn commands

---
+++ action-turncommandintent.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import sys
+import io
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import requests
+import socketio
+sio = socketio.Client()
+sio.connect('http://localhost')
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section: {option_name: option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding='utf-8') as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def session_started(hermes, session_started_message):
+    session_id = session_started_message.session_id
+    custom_data = session_started_message.custom_data
+    sentence = 'Started a new session of' + \
+        session_id + ' with data ' + str(custom_data)
+    print(sentence)
+
+
+def session_ended(hermes, session_ended_message):
+    session_id = session_ended_message.session_id
+    session_site_id = session_ended_message.site_id
+    print('Ending session...')
+ 
+def subscribe_intent_turncommand(hermes, intent_message):
+     conf = read_configuration_file('config.ini')
+     if len(intent_message.slots.TURN_COMMAND_SLOT) > 0:
+         turn_command = intent_message.slots.TURN_COMMAND_SLOT.first().value
+         sio.emit('turn_command_from_user', turn_command)
+         hermes.publish_end_session(intent_message.session_id, 'Ok, Moving ' + turn_command)
+     else:
+         hermes.publish_end_session(intent_message.session_id, "It doesn't work like that, try again please")
+
+if __name__ == "__main__":
+    with Hermes('localhost:1883') as h:
+        h.subscribe_intent('gunasekartr:turncommandintent', subscribe_intent_turncommand) \
+            .subscribe_session_ended(session_ended) \
+            .subscribe_session_started(session_started) \
+            .start()
