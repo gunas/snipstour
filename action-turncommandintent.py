4f3500f69972c63e925ed91f668b31d58b7e9443
Gunasekar <gunasekar.thambusamy@gmail.com>
2019-01-28 12:40:58 +0000

    dialogue revised

--- action-turncommandintent.py
+++ action-turncommandintent.py
@@ -42,7 +42,7 @@ def subscribe_intent_turncommand(hermes, intent_message):
          socketIO.emit('rotation_command', turn_command)
          lastcommand = 'rotation_command@'+ turn_command
          requests.get('http://localhost/sstore?lastcommand='+ lastcommand + '&sid=' + intent_message.session_id)
-         hermes.publish_continue_session(intent_message.session_id, 'Turing '+ turn_command + ', Would you like to turn again?', ['gunasekartr:continueintent']);
+         hermes.publish_continue_session(intent_message.session_id, 'Turing '+ turn_command, ['gunasekartr:continueintent']);
      else:
          hermes.publish_end_session(intent_message.session_id, "It doesn't work like that, try again please")
 
@@ -52,7 +52,7 @@ def subscribe_intent_movecommand(hermes, intent_message):
          socketIO.emit('move_command', move_command)
          lastcommand = 'move_command@'+ move_command
          requests.get('http://localhost/sstore?lastcommand='+ lastcommand + '&sid=' + intent_message.session_id)
-         hermes.publish_continue_session(intent_message.session_id, 'Moving ' + move_command + ', Would you like to move again?', ['gunasekartr:continueintent']);
+         hermes.publish_continue_session(intent_message.session_id, 'Moving ' + move_command, ['gunasekartr:continueintent']);
      else:
          hermes.publish_end_session(intent_message.session_id, "It doesn't work like that, try again please")
 
@@ -63,7 +63,7 @@ def subscribe_intent_continue(hermes, intent_message):
              lastcommand = requests.get('http://localhost/lastcommand?sid=' + intent_message.session_id)
              last_intent_command, action = lastcommand.text.split("@")
              socketIO.emit(last_intent_command, action)
-             hermes.publish_continue_session(intent_message.session_id, 'Ok, Would you like to repeat?', ['gunasekartr:continueintent']);
+             hermes.publish_continue_session(intent_message.session_id, 'Ok', ['gunasekartr:continueintent']);
          else:
              hermes.publish_end_session(intent_message.session_id, "That's fine.");     
      else:
